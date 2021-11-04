7551c0486caeca2635013796c94f8c908e603bc7
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-11-04 06:33:15 -0400

    Creates custom hooks function using array form for useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,75 @@
-import './App.css';
+import React, {useState} from 'react';
+
+import "./App.css";
+
+function CustomHooksUsingArraysForm(initialState) {
+  const [value, setValue] = useState(initialState);
+  
+  function onChange(e) {
+    setValue(e.target.value)
+  };
+
+  function clearInput() {
+    setValue("");
+  }
+
+  return [value, onChange, clearInput];
+}
 
 function App() {
+  const [firstName, firstNameOnChange, clearFirstNameInput] = CustomHooksUsingArraysForm("");
+  const [lastName, lastNameOnChange, clearLastNameInput] = CustomHooksUsingArraysForm("");
+  const [email, emailOnChange, clearEmailInput] = CustomHooksUsingArraysForm("");
+  const [telephone, telephoneOnChange, clearTelephoneInput] = CustomHooksUsingArraysForm("");
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    console.log(firstName)
+    console.log(lastName)
+    console.log(email)
+    console.log(telephone)
+    clearFirstNameInput()
+    clearLastNameInput()
+    clearEmailInput()
+    clearTelephoneInput()
+  }
+
   return (
-    <div className="App">
-    Hello, It's Me
+    <div className="app">
+      <form onSubmit={handleOnSubmit}>
+        <input 
+          type="text" 
+          placeholder="first name"
+          value={firstName}
+          onChange={(e) => firstNameOnChange(e)}
+        />
+        <br />
+        <input 
+          type="text" 
+          placeholder="last name"
+          value={lastName}
+          onChange={(e) => lastNameOnChange(e)}
+        />
+        <br />
+        <input 
+          type="email" 
+          placeholder="email"
+          value={email}
+          onChange={(e) => emailOnChange(e)}
+        />
+        <br />
+        <input 
+          type="tel" 
+          placeholder="enter contact number" 
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+          value={telephone}
+          onChange={(e) => telephoneOnChange(e)}
+        />
+        <br />
+        <button type="Submit">Submit</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
